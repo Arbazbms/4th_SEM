6d6183091922a7c565c33d3d7a884a19ffaf9a00
Arbaz Ahmed <56014061+Arbazbms@users.noreply.github.com>
2020-03-06 12:43:14 +0530

    Update p4SF.sql

--- DBMS/p4SF.sql
+++ DBMS/p4SF.sql
@@ -1,5 +1,6 @@
 create database StudentFaculty;
 use StudentFaculty;
+show databases;
 
 create table student(
 	snum int not null,
@@ -40,4 +41,37 @@ create table enrolled (
  show tables;
  drop table student;
  
+ 
+insert into faculty values(11,'Dr NV',02);
+insert into faculty values(12,'Dr GS',01);
+insert into faculty values(13,'Dr UV',02);
+insert into faculty values(14,'Dr BJ',02);
+insert into faculty values(15,'NM',01)
+ 
+ 
+insert into student VALUES(101,'Yaseen','CS','JR',19);
+insert into student VALUES(102,'Arbaz','CS','SR',20);
+insert into student VALUES(103,'Abhishek','IS','SR',21);
+insert into student VALUES(104,'Chirag','CS','JR',20);
+insert into student VALUES(105,'Chiru','IS','SR',21);
+select * from student;
+
+
+
+insert into class values('OO_Java','09:15:00',407,11);
+insert into class values('TFCS','09:15:00',402,12);
+insert into class values('ADA','10:10:00',404,13);
+insert into class values('DBMS','01:00:00',128,14);
+insert into class values('OS','02:00:00',206,15);
+
+
+insert into enrolled values(101,'OO_Java');
+insert into enrolled values(102,'TFCS');
+insert into enrolled values(103,'ADA');
+insert into enrolled values(104,'DBMS');
+insert into enrolled values(105,'OS');
+
+/* Find the name of all the juniors(level - jr) who are enrolled in a class taught by somefname*/
+select sname , fname from student,enrolled where
+
  
